6b4b661cc518524512a449b30a442019b05770ea
Guillaume BELLEGUIC <guillaume.belleguic@with.fr>
2026-01-19 12:15:45 +0100

    domain

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  base: '/pokedex/',
+  base: 'https://pokedex.belleguic.com/',
   plugins: [react()],
   test: {
     projects: [{
